576125c5d915163c3814b13648c5d70e75ca8ee9
ils-akashp <a.paliwal@thesynapses.com>
2024-04-05 15:46:11 +0530

    third party user signin method add

--- Controllers/users.js
+++ Controllers/users.js
@@ -5,10 +5,9 @@ import jwt from "jsonwebtoken";
 export const signin = async (req, res) => {
   console.log("signin", req.body);
   const { email, password } = req.body;
-  console.log('hashedpassword', await bcrypt.hash(password, 12))
   try {
     const existingUser = await User.findOne({ email });
-    console.log('existingUser====',existingUser)
+    console.log("existingUser====", existingUser);
     if (!existingUser) {
       return res.status(404).json({ message: "User doesn't exist" });
     }
@@ -17,7 +16,7 @@ export const signin = async (req, res) => {
       password,
       existingUser.password
     );
-    console.log('isPasswordCorrect====',isPasswordCorrect)
+    console.log("isPasswordCorrect====", isPasswordCorrect);
     if (!isPasswordCorrect)
       return res.status(400).json({ message: "Invlaid Credentials!" });
 
@@ -26,7 +25,7 @@ export const signin = async (req, res) => {
       "test",
       { expiresIn: "1h" }
     );
-    console.log('token====',token)
+    console.log("token====", token);
     res.status(200).json({ result: existingUser, token });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
@@ -45,22 +44,60 @@ export const signup = async (req, res, next) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-    console.log(hashedPassword);
-
-    const result = await User.create({
-      fullName: fullName,
-      email: email,
-      password: hashedPassword,
-    });
-
-    const token = jwt.sign({ email: result.email, id: result._id }, "test", {
-      expiresIn: "1h",
-    });
-
-    res.status(200).json({ result, token });
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const result = await User.create({
+        fullName: fullName,
+        email: email,
+        password: hashedPassword,
+      });
+      const token = jwt.sign({ email: result.email, id: result._id }, "test", {
+        expiresIn: "1h",
+      });
+      res.status(200).json({ result, token });
+    } else {
+      const result = await User.create({
+        fullName: fullName,
+        email: email,
+      });
+      const token = jwt.sign({ email: result.email, id: result._id }, "test", {
+        expiresIn: "1h",
+      });
+      res.status(200).json({ result, token });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "User creation failed" });
+  }
+};
+
+export const thirdPartyUser = async (req, res) => {
+  console.log(req.body);
+  const { fullName, email } = req.body;
+  try {
+    const existingUser = await User.findOne({
+      email,
+    });
+    console.log('existingUser=======',existingUser)
+    if (existingUser) {
+      const token = jwt.sign(
+        { email: existingUser.email, id: existingUser._id },
+        "test",
+        { expiresIn: "1h" }
+      );
+      console.log("token====", token);
+      res.status(200).json({ result: existingUser, token });
+    } else {
+      const result = await User.create({
+        fullName: fullName,
+        email: email,
+      });
+      const token = jwt.sign({ email: result.email, id: result._id }, "test", {
+        expiresIn: "1h",
+      });
+      res.status(200).json({ result, token });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error creating user" });
   }
 };
--- Routes/user.js
+++ Routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signin, signup } from "../Controllers/users.js";
+import { signin, signup, thirdPartyUser } from "../Controllers/users.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/thirdpartysignin", thirdPartyUser);
 
 export default router;
